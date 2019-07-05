d6961ec52553d2dc03a124d1ce4f4ee005e42fd9
Lychee <lycheeEng@hotmail.com>
2019-07-05 15:59:45 +0800

    [Result] add result component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import './Result.scss';
+
 function Result() {
-  return <div>Result</div>
+  return (
+    <div className='result'>
+      {/* pass an array */}
+    </div>);
 }
 
 export default Result;
